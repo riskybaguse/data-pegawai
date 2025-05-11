5de2e368b3e0a999a0d996c6d5b7e4daaf2cd53f
riskybaguse <riskybaguse474@gmail.com>
2025-05-11 13:27:21 +0700

    id pegawai

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -27,9 +27,9 @@
             <td>{{ $p->pegawai_umur}}</td>
             <td>{{ $p->pegawai_alamat}}</td>
             <td>
-                <a href="/pegawai/edit/{{ $p->id }}">Edit</a>
+                <a href="/pegawai/edit/{{ $p->pegawai_id }}">Edit</a>
                 |
-                <a href="/pegawai/hapus/{{ $p->id }}">Hapus</a>
+                <a href="/pegawai/hapus/{{ $p->pegawai_id }}">Hapus</a>
             </td>
         </tr>
         @endforeach
